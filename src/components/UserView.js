e2a4af103f5483b79e8dcd8f666ee7d498c3b189
HenryFerreira <henryferreira133@gmail.com>
2022-04-01 15:10:57 -0300

    Se agregaron las rutas a UserVeiw.js

--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,13 +1,31 @@
 import React from "react";
 
+import { 
+  BrowserRouter, 
+  Routes, 
+  Route, 
+  Link 
+} from "react-router-dom";
+
+import DefaultPage from "./pages/DefaultPage"
 function UserView() {
   return (
     <div>
-      Hola, usuario
-      
-
-
-
+      Hola, user
+      <div>
+        <BrowserRouter>
+          <nav>
+            <div>
+              <Link to="/">Inicio</Link>
+              {"  "}
+            </div>
+          </nav>
+          <br/>
+          <Routes>
+            <Route path="/" element={<DefaultPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
