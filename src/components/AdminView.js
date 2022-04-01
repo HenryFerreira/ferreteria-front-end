6e01cfbac19711a0c1c8a424edd24e5a4ea0e614
HenryFerreira <henryferreira133@gmail.com>
2022-04-01 13:41:05 -0300

    Se implemento los Routers en AdminView

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import ListarClientes from "./ClienteView";
+import DefaultPage from "./DefaultPage"
 
 function AdminView() {
   return (
     <div>
       Hola, admin
-      <ListarClientes />
+      <div>
+        <BrowserRouter>
+          <nav>
+            <div>
+              <Link to="/">Inicio</Link>
+              {"  "}
+              <Link to="/cliente">Listar Cliente</Link>
+            </div>
+          </nav>
+          <Routes>
+            <Route path="/" element={<DefaultPage />} />
+            <Route path="/cliente" element={<ListarClientes />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 
